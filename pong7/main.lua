7cbcdb5b971b57afe14ab18f11c1282a585f7f82
Zack-Kwalski <shikharmohangautam3@gmail.com>
2020-05-05 18:32:45 +0530

    (collision_update)

---
+++ pong7/main.lua
@@ -0,0 +1,151 @@
+push = require 'push'
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+paddle_speed = 200
+
+
+ nFont =  love.graphics.newFont('font.ttf',10)
+ n2Font = love.graphics.newFont('font.ttf',20)
+ playername = ''
+ count = 0
+ 
+ function love.load()
+   love.graphics.setDefaultFilter('nearest','nearest')
+   love.window.setTitle('Zack Kwalski (Shikhar)')
+   math.randomseed(os.time())
+   ball = Ball(VIRTUAL_WIDTH/2 - 2,VIRTUAL_HEIGHT/2 - 2,4,4)
+   Player1 = Paddle(5,0,5,30)
+   Player2 = Paddle(VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-30,5,30)
+   P1Score = 0
+   P2Score = 0
+  
+  love.graphics.setFont(nFont)
+
+  
+ push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
+   fullscreen = false,
+   resizable = false,
+   vsync = true
+  })
+   gamestate = 'start'
+  end
+
+  function love.update(dt)
+  
+   if gamestate == 'play' then 
+
+     if ball:collision(Player1) then
+        ball.x = Player1.x + 5
+        ball.xspeed = - ball.xspeed*1.1
+        if ball.yspeed < 0 then
+                 ball.yspeed = - math.random(10,150)
+        else
+                 ball.yspeed = math.random(10,150)
+        end
+     end
+
+     if ball:collision(Player2) then
+          ball.x = Player2.x - 4
+          ball.xspeed = -ball.xspeed*1.1
+          if ball.yspeed < 0 then
+                 ball.yspeed = - math.random(10,150)
+        else
+                 ball.yspeed = math.random(10,150)
+        end
+     end
+
+     if ball.y<=0 then
+         ball.y = 0 
+         ball.yspeed = - ball.yspeed
+     end
+
+     if ball.y >= VIRTUAL_HEIGHT-4 then
+         ball.y = VIRTUAL_HEIGHT - 4
+         ball.yspeed = - ball.yspeed
+     end
+   end
+
+   if  love.keyboard.isDown('w') then
+       Player1.speed = - paddle_speed
+  elseif love.keyboard.isDown('s') then
+       Player1.speed =  paddle_speed
+  else 
+       Player1.speed = 0
+   end
+
+  if  love.keyboard.isDown('up') then
+       Player2.speed = - paddle_speed
+  elseif love.keyboard.isDown('down') then
+       Player2.speed = paddle_speed
+  else 
+       Player2.speed = 0 
+  end
+
+   if gamestate == 'play' then
+       ball:update(dt)
+   end
+
+   Player1:update(dt)
+   Player2:update(dt)
+   
+ end
+
+
+ function love.keypressed(key)
+    if key == 'escape' then
+           love.event.quit()
+    elseif key == 'enter'   or key == 'return'  then
+        count = count + 1
+        if gamestate == 'start' then
+        gamestate = 'play'
+        else 
+        gamestate = 'start'
+        ball:reset()
+        end
+      else
+        if count == 0 and string.byte(key)==65 then--and string.byte(key)<=90) or (string.byte(key)>=97 and string.byte(key)<=122)) then
+        playername = playername ..  tostring(key)
+      end
+     end
+  end
+
+function love.draw()
+   
+    push:apply('start')
+    love.graphics.setFont(nFont)
+    if gamestate == 'play' then
+      love.graphics.print(playername .. "'s Score ",VIRTUAL_WIDTH/2 + 1,5)
+    else 
+      love.graphics.setFont(nFont)
+      love.graphics.print("Enter your name to start")
+      love.graphics.setFont(n2Font)
+      love.graphics.print(playername,VIRTUAL_WIDTH/2 + 1,VIRTUAL_HEIGHT/2 - 20)
+    end
+    Player1:render()
+    Player2:render()
+    ball:render()
+    love.graphics.setFont(n2Font)
+    --love.graphics.print(P1Score,VIRTUAL_WIDTH/2-18,15)
+    love.graphics.print(P2Score,VIRTUAL_WIDTH/2+3,15)
+    push:apply('end')
+  end
+
+ 
+
+function displayFPS()
+    love.graphics.setFont(n2Font)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()),10,10)
+end
+
+ 
+
+        
+ 
+ 
